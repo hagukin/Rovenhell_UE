5739def260eae4d500007d700b69f290527425de
hagukin <hagukin@naver.com>
2023-08-27 23:03:38 +0900

    [feat] 클라, 로직서버 분리 테스트 환경 구축

--- Source/IocpGame/IocpGameGameMode.h
+++ Source/IocpGame/IocpGameGameMode.h
@@ -16,6 +16,18 @@ public:
 	AIocpGameGameMode();
 
 public:
+	UFUNCTION(BlueprintCallable, Category = "ExecType")
+	void SetAsClient() { GameExecType = ExecTypeEnum::CLIENT; }
+
+	UFUNCTION(BlueprintCallable, Category = "ExecType")
+	void SetAsClientHeadless() { GameExecType = ExecTypeEnum::CLIENT_HEADLESS; }
+
+	UFUNCTION(BlueprintCallable, Category = "ExecType")
+	void SetAsLogicServer() { GameExecType = ExecTypeEnum::SERVER; }
+
+	UFUNCTION(BlueprintCallable, Category = "ExecType")
+	void SetAsLogicServerHeadless() { GameExecType = ExecTypeEnum::SERVER_HEADLESS; }
+
 	ExecType GameExecType; // TODO: ExecType 변경 가능하게 하는 기능
 };
 
